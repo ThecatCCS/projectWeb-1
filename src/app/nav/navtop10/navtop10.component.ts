da04f0350b2fb24fd85453ab723a2b629cb634bf
CAT <bigstirnabon@gmail.com>
2024-03-02 01:53:59 +0700

    send

--- src/app/nav/navtop10/navtop10.component.ts
+++ src/app/nav/navtop10/navtop10.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-navtop10',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule,CommonModule],
   templateUrl: './navtop10.component.html',
   styleUrl: './navtop10.component.scss'
 })
